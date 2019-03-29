b0a1aa71d9156b29154265c079205fa4935779ef
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2019-03-29 15:18:09 +0600

    Create Spring Boot Project and Add Dependency.

---
+++ Department Project/src/main/java/org/JU/deptofCSE/Department/Project/DepartmentProjectApplication.java
@@ -0,0 +1,13 @@
+package org.JU.deptofCSE.Department.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentProjectApplication.class, args);
+	}
+
+}
