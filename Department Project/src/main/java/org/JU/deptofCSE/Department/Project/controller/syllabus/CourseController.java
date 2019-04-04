796b704558307c950ba4a530a78308e74af0ab90
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2019-04-05 04:43:36 +0600

    Controller

--- Department Project/src/main/java/org/JU/deptofCSE/Department/Project/controller/syllabus/CourseController.java
+++ Department Project/src/main/java/org/JU/deptofCSE/Department/Project/controller/syllabus/CourseController.java
@@ -2,6 +2,7 @@ package org.JU.deptofCSE.Department.Project.controller.syllabus;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,7 +11,13 @@ public class CourseController {
 
     @RequestMapping(value = "/addNewCourse")
     public ModelAndView getCourseData() {
-        ModelAndView courseInputPage = new ModelAndView();
+        ModelAndView courseInputPage = new ModelAndView("AddNewCourse.html");
+
         return courseInputPage;
+    }
+
+    @RequestMapping(value = "/saveCourse", method = RequestMethod.POST)
+    public ModelAndView saveCourse() {
+        return null;
     }
 }
