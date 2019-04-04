d99fc3dddcae49b0144ee1c4e4bbff62c73e55b3
shamimjucse <shamimimtiaz9@gmail.com>
2019-04-05 02:24:26 +0600

    class change

--- Department Project/src/main/java/org/JU/deptofCSE/Department/Project/model/syllabus/Syllabus.java
+++ Department Project/src/main/java/org/JU/deptofCSE/Department/Project/model/syllabus/Syllabus.java
@@ -1,4 +1,27 @@
 package org.JU.deptofCSE.Department.Project.model.syllabus;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "Syllabus")
 public class Syllabus {
+
+    @XmlElement(name = "Semesters")
+    List<Semesters> semesters;
+
+    public List<Semesters> get_Semesters() {
+        return semesters;
+    }
+
+    public void setSemesters(List<Semesters> semesters) {
+        this.semesters = semesters;
+    }
+
+    @Override
+    public String toString() {
+        return "Syllabus{" +
+                "semesters=" + semesters +
+                '}';
+    }
 }
