bf018644efc516869b6b8b66659aa12ea5e30fb0
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2019-04-10 01:32:31 +0600

    Update View

--- Department Project/src/main/java/org/JU/deptofCSE/Department/Project/repository/TeacherRepository.java
+++
@@ -1,7 +0,0 @@
-package org.JU.deptofCSE.Department.Project.repository;
-
-import org.JU.deptofCSE.Department.Project.model.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
-}
